253778ed5eadc73e8dba8a16396b2a7ea38780e0
progundeike <89243251+progundeike@users.noreply.github.com>
2026-03-25 21:07:03 +0900

    Basic認証を削除

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -6,7 +6,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>index.blade.php</title>
+    <title>支援士対策室</title>
 
     <!-- Scripts -->
     @viteReactRefresh
