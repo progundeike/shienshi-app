02e1ac5f9a6aa5bd31f922c43a92294c25d87298
progundeike <89243251+progundeike@users.noreply.github.com>
2026-07-09 11:28:49 +0900

    未ログイン時のリダイレクトを修正

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -10,13 +10,21 @@ use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->statefulApi();
+
+        $middleware->redirectGuestsTo(function (Request $request) {
+            if ($request->is('api/*')) {
+                return null; // APIリクエストの場合はリダイレクトしない
+            }
+
+            return '/login';
+        });
 
         // 管理者権限用のミドルウェアを追加
         $middleware->alias([
