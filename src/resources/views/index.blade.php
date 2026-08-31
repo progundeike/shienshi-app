6fef473af3f6efafff1be81c01ae06eb175db228
progundeike <89243251+progundeike@users.noreply.github.com>
2026-08-31 09:14:56 +0900

    google analysticsの設定を追加

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -4,6 +4,16 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-K3X3ZLMT9G"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-K3X3ZLMT9G');
+    </script>
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>支援士対策室</title>
